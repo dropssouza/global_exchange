13478fdffc3e9f09a3cda894b5825864d5a84835
Pedro Souza <pedrohenrique_2010@hotmail.com>
2026-02-03 22:32:51 -0300

    refactor: corrigir requisicao api 03-02-25

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,35 @@
-import { use, useState } from "react";
-import "./App.css";
-import Header from "./components/Header/Header.jsx";
-import Main from "./components/Main/Main.jsx";
-import Coins from "./components/Coins/Coins.jsx";
-import Button from "./components/Button/Button.jsx";
+import { use, useState } from 'react';
+import './App.css';
+import Header from './components/Header/Header.jsx';
+import Main from './components/Main/Main.jsx';
+import Coins from './components/Coins/Coins.jsx';
+import Button from './components/Button/Button.jsx';
 
 function App() {
-  const [currency1, setCurrency1] = useState("USD");
-  const [currency2, setCurrency2] = useState("BRL");
-  const [value1, setValue1] = useState("");
-  const [value2, setValue2] = useState("");
+  const [currency1, setCurrency1] = useState('USD');
+  const [currency2, setCurrency2] = useState('BRL');
+  const [value1, setValue1] = useState('');
+  const [value2, setValue2] = useState('');
   const [loading, setLoading] = useState(false);
 
-  const API_URL = "https://currency-conversion-srm9.onrender.com";
+  const API_URL = 'https://currency-conversion-srm9.onrender.com';
+  const URL = `http://127.0.0.1:8000/converter?moeda_origem=${currency1}&moeda_destino=${currency2}&valor=${value1}`;
 
   async function calculate() {
     if (!value1) {
-      alert("Type your value to convert!");
+      alert('Type your value to convert!');
       return;
     }
 
     setLoading(true);
 
     try {
-      const response = await fetch(
-        `${API_URL}/converter?moeda_origem=${currency1}&moeda_destino=${currency2}&valor=${value1}`
-      );
+      const response = await fetch({ URL });
       const data = await response.json();
       setValue2(data.valor_convertido);
     } catch (error) {
-      console.error("Error during the conversion", error);
-      alert("Error connecting to the server");
+      console.error('Error during the conversion', error);
+      alert('Error connecting to the server');
     }
 
     setLoading(false);
